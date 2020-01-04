5d7bffa9f667f8f9628e372cd0ceb335c42e8541
muhammadZihad <zihad.muhammadar@gmail.com>
2020-01-05 04:37:24 +0600

    vacation blade edited

--- resources/views/Vacation/ask-vacation.blade.php
+++ resources/views/Vacation/ask-vacation.blade.php
@@ -5,27 +5,42 @@
 {{-- @if($errors->has())
     @foreach ($errors->all() as $error)
         <div>{{ $error }}</div>
-    @endforeach
+@endforeach
 @endif --}}
 
-<form method="POST" action="{{ route('submitVacation') }}">
-    {{ csrf_field() }}
-    <div class="form-row">
-      <div class="form-group col-md-4">
-        <label for="inputEmail4">From</label>
-        <input type="date" class="form-control" id="inputEmail4" placeholder="From" name="from">
-      </div>
-      <div class="form-group col-md-4">
-        <label for="inputEmail4">To</label>
-        <input type="date" class="form-control" id="inputEmail4" placeholder="To" name="to">
-      </div>
+<div class="row">
+  <div class="col-12">
+    <div class="main-card card">
+      <div class="card-header">Ask for vacation</div>
+      <form method="POST" action="{{ route('submitVacation') }}">
+        {{ csrf_field() }}
+        <div class="card-body">
+          <div class="form-row">
+            <div class="form-group col-md-4">
+              <label for="inputEmail4">From</label>
+              <input type="date" class="form-control" id="inputEmail4" placeholder="From" name="from">
+            </div>
+            <div class="form-group col-md-4">
+              <label for="inputEmail4">To</label>
+              <input type="date" class="form-control" id="inputEmail4" placeholder="To" name="to">
+            </div>
 
-      <div class="form-group col-md-8">
-        <label for="exampleFormControlTextarea1">Describe vacation asking reason</label>
-        <textarea class="form-control" id="exampleFormControlTextarea1" rows="8" style="padding:10px" name="description"></textarea>
-      </div>
+            <div class="form-group col-md-8">
+              <label for="exampleFormControlTextarea1">Describe vacation asking reason</label>
+              <textarea class="form-control" id="exampleFormControlTextarea1" rows="8" style="padding:10px"
+                name="description"></textarea>
+            </div>
 
+          </div>
+        </div>
+        <div class="card-footer"> <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
     </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
+  </div>
+</div>
+</div>
+
+
+
+
 @endsection
